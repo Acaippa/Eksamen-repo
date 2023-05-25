eccf6621e4c97e6718f7ab4435a49980ccea578a
Acaippa <olivernils8@gmail.com>
2023-05-25 21:44:42 +0200

    Added home page

--- nettside/views.py
+++ nettside/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Laptop
 
 # Create your views here.
+def home_view(request):
+    # Prosseser index.html med alle laptoppene som context.
+    return render(request, "index.html", {"laptops" : Laptop.objects.all()}) # SELECT * FROM Laptop
