941b6e693ee82c544a3f3848312efcaa3a743375
Acaippa <olivernils8@gmail.com>
2023-05-26 12:40:08 +0200

    Added detail view

--- nettside/models.py
+++ nettside/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Samme som "CREATE TABLE Laptop"
 class Laptop(models.Model):
     # leverandor varchar(20)
-    leverandor = models.CharField(max_length=20)
+    merke = models.CharField(max_length=20)
     modell = models.CharField(max_length=20)
 
     # IntegerField() trenger ikke max_length keywordet
@@ -21,5 +21,10 @@ class Laptop(models.Model):
     # blank og null gør at dette feltet blir ikke-obligatorisk å fylle ut.
     notater = models.CharField(max_length=1000, blank=True, null=True)
 
+    lagringskapasitet = models.IntegerField()
+    CPU = models.CharField(max_length=100)
+    RAM = models.CharField(max_length=100)
+    operativsystem = models.CharField(max_length=100)
+
     def __str__(self):
-        return f"{self.leverandor} {self.modell} {self.status}"
+        return f"{self.merke} {self.modell} {self.status}"
--- nettside/views.py
+++ nettside/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Laptop
+from .forms import LaptopForm
 
 # Create your views here.
 def home_view(request):
+    form = LaptopForm()
+
+    if request.method == "POST":
+        form = LaptopForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            form = LaptopForm()
+
     # Prosseser index.html med alle laptoppene som context.
-    return render(request, "index.html", {"laptops" : Laptop.objects.all()}) # SELECT * FROM Laptop
+    return render(request, "index.html", {"laptops" : Laptop.objects.all(), "form" : form}) # SELECT * FROM Laptop
+
+
